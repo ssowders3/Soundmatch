7a5b27da5883f98b5a8237f67b63584497a30d73
dtrementozzi3 <dtremnz@gatech.edu>
2018-10-14 13:47:31 -0400

    Scrollview with text

--- SoundMatch_Frontend/App.js
+++ SoundMatch_Frontend/App.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={{height: 200, backgroundColor: 'steelblue'}} />
-        <ScrollView style={{height: 200, backgroundColor: 'skyblue'}} />
-        <ScrollView style={{height: 200, backgroundColor: 'powderblue'}} />
-        <ScrollView style={{height: 200, backgroundColor: 'white'}} />
-      </View>
-    );
-  }
-}
+import { View, ScrollView, ListView, StyleSheet, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'stretch',
+    marginTop: 20,
   },
 });
+
+class ListViewDemo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['Darrens Music', 'Prabhaths Music', 'Gautums Music', 'Katies Music']),
+    };
+  }
+  render() {
+    return (
+
+      <ScrollView style={styles.container}>
+        <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={(data) => <View><Text>{data}</Text></View>}
+        />
+        <View style={{height: 200, backgroundColor: 'steelblue'}}>
+          <Text>ajskldjf</Text>
+        </View>
+        <View style={{height: 200, backgroundColor: 'skyblue'}} />
+        <View style={{height: 200, backgroundColor: 'powderblue'}} />
+        <View style={{height: 200, backgroundColor: 'white'}} />
+      </ScrollView>
+
+    );
+  }
+}
+///////
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     alignItems: 'stretch',
+//   },
+// });
+
+export default ListViewDemo;
+
